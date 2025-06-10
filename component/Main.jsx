1b2806e9a8508e9badde7be8adce2f8f10a9ef10
MaryKaratu <Karatumary85@gmail.com>
2025-06-10 09:44:51 +0100

    Event-Handler

--- component/Main.jsx
+++ component/Main.jsx
@@ -1,14 +1,26 @@
 export default function Main() {
+  const ingredients = ["chicken", "Tomatoes", "Perrper"];
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Form Submitted");
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="e.g oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add Ingredient</button>
       </form>
+      <ul>
+        {ingredients.map((ing) => (
+          <li key={ing}>{ing}</li>
+        ))}
+      </ul>
     </main>
   );
 }
