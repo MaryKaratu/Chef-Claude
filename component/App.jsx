670d9544ced357b1def61852d3378d6816cc3040
MaryKaratu <Karatumary85@gmail.com>
2025-06-12 14:27:08 +0100

    Refactor Component

--- component/App.jsx
+++ component/App.jsx
@@ -1,5 +1,7 @@
 import Header from "../Header";
 import Main from "./Main";
+import IngredientList from "./ingredientList";
+import ClaudeRecipe from "./claudeRecipe";
 
 export default function App() {
   return (
--- component/Main.jsx
+++ component/Main.jsx
@@ -1,31 +1,38 @@
 import { useState } from "react";
+import ClaudeRecipe from "./claudeRecipe";
+import IngredientList from "./ingredientList";
 
 export default function Main() {
-  const [ingredient, setIngredient] = useState([]);
+  const [ingredients, setIngredient] = useState([]);
+  const [recipeShown, setRecipeShown] = useState(false);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
+  function addIngredient(formData) {
     const newIngredient = formData.get("ingredient");
     setIngredient((prevIngr) => [...prevIngr, newIngredient]);
   }
 
+  function toggleRecipeShown() {
+    setRecipeShown((prev) => !prev);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form" onSubmit={handleSubmit}>
+      <form className="add-ingredient-form" action={addIngredient}>
         <input
           type="text"
           placeholder="e.g oregano"
           aria-label="Add ingredient"
           name="ingredient"
         />
-        <button onSubmit={handleSubmit}>Add Ingredient</button>
+        <button>Add Ingredient</button>
       </form>
-      <ul>
-        {ingredient.map((ing, index) => (
-          <li key={index}>{ing}</li>
-        ))}
-      </ul>
+      {ingredients.length > 0 ? (
+        <IngredientList
+          ingredient={ingredients}
+          toggleRecipeShown={toggleRecipeShown}
+        />
+      ) : null}
+      {recipeShown && <ClaudeRecipe />}
     </main>
   );
 }
